adfbc0c3bf781cb11cd5501f3b150356f0b048b0
suy9508 <126954577+suy9508@users.noreply.github.com>
2023-03-04 14:51:17 +0400

    Добавила слайдбар, навбар, добавила вотсап, футер, таймер
    Добавила слайдбар ,навбар, вотсап, футер, таймер, стили и скрипт для таймера

---
+++ script.js
@@ -0,0 +1,36 @@
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minsElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('секунды');
+
+
+const targetDate = "8 Mar 2023";
+
+function countdown() {
+		// инициализация значений
+    const newTargetDate = new Date(targetDate);
+    const currentDate = new Date();
+
+    const totalSeconds = new Date(newTargetDate - currentDate) / 1000;
+
+	 // блок вычислений
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+	 // присваиваем в hrml значения
+    daysElement.innerHTML = formatTime(days);
+    hoursElement.innerHTML = formatTime(hours);
+    minsElement.innerHTML = formatTime(mins);
+    secondsElement.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time;
+}
+
+// initial call
+countdown();
+
+setInterval(countdown, 1000);
